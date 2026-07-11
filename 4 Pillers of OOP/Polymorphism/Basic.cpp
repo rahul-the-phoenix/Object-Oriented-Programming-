44ed4bab351176cbac74b2105a6ac0386c286b77
Rahul Manna <r4236499@gmail.com>
2026-07-11 13:38:37 +0530

    Implement operator overloading in Complex class
    Added operator overloading for Complex class and provided use cases for operator overloading.

--- 4 Pillers of OOP/Polymorphism/Basic.cpp
+++ 4 Pillers of OOP/Polymorphism/Basic.cpp
@@ -4,8 +4,6 @@
  all the same fiontions if stay on parent or class then it is Runtime Polymorphism : Function Overridding
  all the same functions if stay on different independent class then it's not a polymorphism (a1.display() / b1.display() ) 
 
-
-
 #include <iostream>
 using namespace std;
 
@@ -38,4 +36,64 @@ int main() {
 //  cout << "circle area (radius 4.5): " << cal.calculateArea(2,4,5,6) << endl;   //error as no function taking 4 eliments 
 
     return 0;
+}
+
+
+
+
+
+
+
+Operator Overloading : to use operators ( + , = , - , * , << , >> ) as a function
+remember : you cannot use this ( . (dot), .* (pointer to member), :: (scope resolution), ?: (ternary), and sizeof ) as a function
+
+Usecase : 
+natural syntax: it lets you use standard symbols like + or - directly on custom objects, making your code look clean and like natural mathematics.
+better readability: it eliminates the need to call clunky, long member functions (like c1.add(c2)), which keeps your codebase intuitive and highly readable.
+ 
+
+#include <iostream>
+using namespace std;
+
+class Complex {
+private:
+    int real, imag;
+
+public:
+    // constructor to initialize values
+    Complex(int r = 0, int i = 0) {
+        real = r;
+        imag = i;
+    }
+
+     // operator overloading using const reference
+    // this allows passing temporary objects like Complex(4, 5)
+   // syntax : return type operator +/-/*/% (const taking referrence of another class &class) 
+    Complex operator + (const Complex& obj) { //for temporary object we must need const keyword , C++ offical rule to be sure that you cannot modify this 
+        Complex temp;
+        temp.real = real + obj.real; // adds real parts
+        temp.imag = imag + obj.imag; // adds imaginary parts
+        return temp;
+    }
+
+
+    void display() {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+int main() {
+    Complex c1(4, 5); // represents 4 + 5i
+    Complex c2(2, 3); // represents 2 + 3i
+
+    Complex c3 = c1 + Complex(4, 5);    //temporary object delete just after its workdone
+      Complex c4 = c1 + c2 ;     //created class 
+
+    cout << "c1 = "; c1.display();
+    cout << "c2 = "; c2.display();
+    cout << "result (c1 + c2) = "; c4.display();
+    cout << "result (c1 + Complex(4,5)) = "; c3.display();
+
+
+    return 0;  // c1 , c2 , c3  objects destroy here 
 }
